b0be40e1faabcce32beef40f04cfeb591b051975
Tanel Viira <tanel.viira@khk.ee>
2015-11-22 00:19:18 +0200

    asjad

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+define("DATABASE_HOSTNAME", '127.0.0.1');
+define("DATABASE_USERNAME", 'root');
+define("DATABASE_PASSWORD", '');
+define("DATABASE_DATABASE", 'waldorf');
---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="et">
+<head>
+    <meta charset="UTF-8">
+    <title>Waldorf</title>
+    <link rel="stylesheet" type="text/css" href="css/css.css">
+</head>
+<body>
+
+<div class="header" style="background-image: url('assets/images/pilt1.jpg')">
+    <div class="heading"></div>
+    <div class="nav">
+        <ul>
+            <li>Avaleht</li>
+            <li>Ühendus</li>
+            <li>Lugemis</li>
+            <li>Koolitused</li>
+            <li>Koolid</li>
+            <li>Lasteaiad</li>
+            <li>Kalender</li>
+        </ul>
+    </div>
+</div>
+
+<div class="body">
+    <div class="pealehe_pilt"><img src="assets/images/p2.jpg" alt="dsa"" ></div>
+    <div class="sisu">
+
+    </div>
+</div>
+
+<div class="footer">
+
+</div>
+
+</body>
+</html>
